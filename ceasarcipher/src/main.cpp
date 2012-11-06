effc7bbc139a06c6cdbf1ca0f53e176bd17a5d5f
Egill Björnsson <egillbjornsson@gmail.com>
2012-11-06 23:17:22 +0000

    Allavegana byrjaður

--- ceasarcipher/src/main.cpp
+++ ceasarcipher/src/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include "ceasar_encrypt.h"
 #include "ceasar_decrypt.h"
+#include <stdlib.h>
+//#include
 
 using namespace std;
 
+char choiceMenu();
+
 int main()
 {
+    choiceMenu();
     cout << "Welcome!" << endl;
 
-    // TODO: Display a menu of choices (1 for encrypt, 2 for decrypt, q for quit)
-    // TODO: Ask the user for menu choice
-    // TODO: If the user chose to quit, exit the program
+    //X TODO: Display a menu of choices (1 for encrypt, 2 for decrypt, q for quit)
+    //X TODO: Ask the user for menu choice
+    //X TODO: If the user chose to quit, exit the program
     // TODO: Ask the user for the number of letters to shift
     // TODO: Ask the user for the string to encrypt/decrypt
     // TODO: Check if input string is legal, keep asking for a new one until it is
@@ -19,5 +24,21 @@ int main()
     // TODO: Repeat (?)
 
     return 0;
+}
+
+char choiceMenu()
+{
+    cout <<"Enter 1 to encrypt a message, 2 to decrypt a message or q to leave the program.";
+    char inputChoice;
+    do
+    {
+        cin >> inputChoice;
+        if(inputChoice!='1' && inputChoice!='2' && inputChoice!='q')
+            cout<<"Please enter a valid input(1, 2 or q)\n";
+    }
+    while(inputChoice!='1' && inputChoice!='2' && inputChoice!='q');
+    if(inputChoice=='q')
+        exit(EXIT_FAILURE);
+    return inputChoice;
 }
 
